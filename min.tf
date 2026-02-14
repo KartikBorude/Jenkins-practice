7277acf946f7c4fbdbf6c27e6e873de364cf8934
KartikBorude <kartikborude001@gmail.com>
2026-02-14 12:10:34 +0530

    Update min.tf

--- min.tf
+++ min.tf
@@ -1,12 +1,12 @@
 provider "aws" {
     region = "ap-southeast-2"
 }
-resource "aws_instance" "kartik" {
+resource "aws_instance" "linux" {
     ami = "ami-0ba8d27d35e9915fb"
-    instance_type = "t3.micro"
+    instance_type = "c7i-flex.large"
     
 
     tags = {
-        Name = "kartik"
+        Name = "linux"
     }
 }
