d3795cd59cab717367e7d08c78c2c4b74f4799bc
KartikBorude <kartikborude001@gmail.com>
2026-02-17 18:45:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "Kartik" {
     ami = "ami-0ba8d27d35e9915fb"
     instance_type = "t3.micro"
-    vpc_security_group_ids = "sg-08e94ae8ffb34b5d5"
+
 
     tags = {
         Name = "Kartik"
